2124401bbef7df697de2a8652111106a53171aef
DG <goodevilbot@gmail.com>
2022-07-15 01:11:01 +0800

    :bug: Hide status bar and bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:bonfire/bonfire.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rescue_my_beauty/player/sprite_sheet_hero.dart';
 import 'package:rescue_my_beauty/rescue_my_beauty_route.dart';
 import 'package:rescue_my_beauty/rescue_my_beauty_routes.dart';
@@ -13,13 +16,23 @@ Future<void> main() async {
     await Flame.device.fullScreen();
   }
   await SpriteSheetHero.load();
-
+  /// 沉浸式状态栏
+  if(!Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent)
+    );
+  }
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,5 +50,21 @@ class MyApp extends StatelessWidget {
         );
       },
     );
+  }
+
+  @override
+  void dispose() {
+    /// 结束恢复隐藏状态栏
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    /// 隐藏状态栏和底部导航栏
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+
   }
 }
